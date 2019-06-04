ff4ac1d9fd9a8f34cf13655da1044e65c26e72b0
Malikai <delfane@live.com>
2019-06-03 23:55:11 -0500

    Adding hello-world.js

---
+++ week-1/hello-world.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title: hello-world
+; Author: GitHub repo
+; Date: 6/3/2019
+; Modified By: <tiffany baker>
+; Description: demonstration of how to run a JavaScript
+application in VS Code
+;===========================================
+*/ 
+
+
+
+console.log("Hello World, my name is Malikai");
